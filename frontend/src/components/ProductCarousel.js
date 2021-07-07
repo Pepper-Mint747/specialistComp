3ed5afd29d60530e63da88ebc09b4ac19fd8f4b0
Brian Omoga <omogabrian2060@outlook.com>
2021-07-08 01:16:31 +0300

    created complete front end and backend

---
+++ frontend/src/components/ProductCarousel.js
@@ -0,0 +1,41 @@
+import React, { useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { Carousel, Image } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import Loader from './Loader'
+import Message from './Message'
+import { listTopProducts } from '../actions/productActions'
+
+const ProductCarousel = () => {
+  const dispatch = useDispatch()
+
+  const productTopRated = useSelector((state) => state.productTopRated)
+  const { loading, error, products } = productTopRated
+
+  useEffect(() => {
+    dispatch(listTopProducts())
+  }, [dispatch])
+
+  return loading ? (
+    <Loader />
+  ) : error ? (
+    <Message variant='danger'>{error}</Message>
+  ) : (
+    <Carousel pause='hover' className='bg-dark border-2'>
+      {products.map((product) => (
+        <Carousel.Item key={product._id}>
+          <Link to={`/product/${product._id}`}>
+            <Image src={product.image} alt={product.name} className={"img-fluid max-with:100% height: auto"} />
+            <Carousel.Caption className='carousel-caption'>
+              <h2>
+                {product.name} (Kshs{product.price})
+              </h2>
+            </Carousel.Caption>
+          </Link>
+        </Carousel.Item>
+      ))}
+    </Carousel>
+  )
+}
+
+export default ProductCarousel
